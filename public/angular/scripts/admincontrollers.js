093835662e025ab211c5430d4021746e0cd56edd
Matthew Murray <mattm.trinsic@gmail.com>
2013-09-26 21:18:14 -0500

    Fixed issue with admin studycard view

--- public/angular/scripts/admincontrollers.js
+++ public/angular/scripts/admincontrollers.js
@@ -85,19 +85,21 @@ app.controller('AdminViewCardsCntrl', function($scope, $http, $routeParams) {
     var url = '/api/studycards?classid='+ $routeParams.classid; // classid should be on query string
     $http.get(url).success( function(studycards) {
         if (studycards.length > 0) {
-            var studycard, studyCardArray = []; // Array of Arrays of Studycards
+            var index = 0, studycard, studyCardArray = []; // Array of Arrays of Studycards
 
             // File cards into StudyCardArray by weekNumber
             for (var i = 0; i < studycards.length; i++) {
 
                 studycard = studycards[i];
                 //console.log('card#: ' + (i+1) + ' week: ' + studycard.weekNum);
-                if (studyCardArray.length >= studycard.weekNum) {
+                if (studyCardArray.length >= studycard.weekNum) { // if current studycard belongs to current week
                     studyCardArray[studycard.weekNum-1].push(studycard); // Add studycard to that week's array
                 } else {
                     // Else move to next week  (where we have card data for that week)
                     //console.log('Going to week ' + studycard.weekNum);
-                    studyCardArray[studycard.weekNum-1] = [studycard]; // create new array for new week
+                    var newWeek = [studycard]; // create new array for new week
+                    newWeek.weekNum = studycard.weekNum;
+                    studyCardArray[index++] = newWeek;
                 }
             }
             $scope.studyCardArray = studyCardArray;
@@ -107,7 +109,12 @@ app.controller('AdminViewCardsCntrl', function($scope, $http, $routeParams) {
 
         // Calculate averages for each week
         for (var i = 0; i < $scope.studyCardArray.length; i++) {
-            calculateWeekAvg(i);
+            // Only do calculations once. If there is a definition at that index then it is already done.
+            if ($scope.weekAvg[i] === undefined) {
+                if  ($scope.studyCardArray[i]) {
+                    $scope.weekAvg[i] = calculateAvg($scope.studyCardArray[i]); // Calculate avg for specific week
+                }
+            }
         }
 
 
@@ -116,16 +123,8 @@ app.controller('AdminViewCardsCntrl', function($scope, $http, $routeParams) {
         window.spinner.stop();
     });
 
-    // Add calculate week average function to the scope so that each week can use it to calculate individual averages
-    var calculateWeekAvg = function(index) {
-        console.log('Calculating week ' + (index+1));
-        // Only do calculations once. If there is a definition at that index then it is already done.
-        if ($scope.weekAvg[index] === undefined) {
-            $scope.weekAvg[index] = calculateAvg($scope.studyCardArray[index]); // Calculate avg for specific week
-        }
-    };
 
-
+    // Return an object that has all the statistics on it
     var calculateAvg = function(studyCards) {
         var totalFreq = 0, totalQuality= 0, totalReadBlock = 0;
 
@@ -136,6 +135,7 @@ app.controller('AdminViewCardsCntrl', function($scope, $http, $routeParams) {
         }
 
         var averages = {};
+        averages.numCards = studyCards.length;
         averages.frequency = Math.round((totalFreq / studyCards.length) *100)/100;
         averages.quality = Math.round( (totalQuality / studyCards.length) * 100) / 100;
         averages.percentRead = Math.round((totalReadBlock / studyCards.length) * 100);
