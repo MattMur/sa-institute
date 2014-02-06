1aac82a3598b1834a9ec56f3c03da878e194160f
Matthew Murray <mattm.trinsic@gmail.com>
2014-02-05 23:50:56 -0600

    Added institute images to jumbotron! Also fixed bug with database console

--- scripts/sqlconn.js
+++ scripts/sqlconn.js
@@ -39,7 +39,7 @@ function handleConnection() {
     });                                     // process asynchronous requests in the meantime.
     // If you're also serving http, display a 503 error.
     connection.on('error', function(err) {
-        console.log('db error', err);
+        console.log('db error'+ err);
         if(err.code === 'PROTOCOL_CONNECTION_LOST') { // Connection to the MySQL server is usually
             handleConnection();                         // lost due to either server restart, or a
         } else {                                      // connnection idle timeout (the wait_timeout
